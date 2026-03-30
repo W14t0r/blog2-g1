3d0e5a57710681c9c382a6bd40335c6e33a4a07f
Michal <egzamin@poczta.pl>
2026-03-30 14:13:27 +0200

    commit niepamietam juz ktory

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
-
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='Published')
 class Post(models.Model):
     class Status(models.TextChoices):
         PUBLISHED = 'PB', 'Published'
@@ -15,7 +17,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     t ="🥹"
-
+    objects = models.Manager()
+    published = PublishedManager
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from blog.models import Post
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        template_name='blog/post/list.html',
+        context={'posts': posts}
+    )
